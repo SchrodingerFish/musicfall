12132b346cf2216e453d611858fd3b53e40ddc11
Schrodinger <39974230+SchrodingerFish@users.noreply.github.com>
2025-12-27 15:03:08 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 静态导出（Cloudflare Workers 需要）
-  output: 'export',
+  distDir: '.next',
   
   // 图片配置（静态导出必须）
   images: {
